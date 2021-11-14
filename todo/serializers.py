bf15a26155c2b2933ca5a9f0bd1f1e467f13f431
ikromjon <ikromjonxusanov>
2021-11-14 19:39:38 +0500

    initial commit

--- todo/serializers.py
+++ todo/serializers.py
@@ -5,14 +5,3 @@ class TodoListSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.TodoList
         fields = "__all__"
-"""
-{
-"title":"Title 1",
-"content": "Content 1",
-"created":"2021-11-12",
-"due_date":"2021-11-15",
-"category":{
-    "id":1
-  }
-}
-"""
